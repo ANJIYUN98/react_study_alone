bade0cf39355a6e3f6b1512eeb348d95edec630f
ANJIYUN98 <155033351+ANJIYUN98@users.noreply.github.com>
2024-09-29 23:38:06 +0900

    20240929 complete

--- tictactoe_study/src/App.js
+++ tictactoe_study/src/App.js
@@ -5,6 +5,7 @@ import Board from "./Components/Board";
 function App() {
   const [history, setHistory] = useState([{squares : Array(9).fill(null) }]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
 
   const calculateWinner = (squares) => {
     const lines = [
@@ -27,7 +28,7 @@ function App() {
   
   }
 
-  const current = history[history.length - 1]; //history 안의 squares는 1,2,3.. 으로 되는데 인덱스는 0,1,2... 이므로 history.length - 1 임
+  const current = history[stepNumber]; //history 안의 squares는 1,2,3.. 으로 되는데 인덱스는 0,1,2... 이므로 history.length - 1 임
   const winner = calculateWinner(current.squares);
 
   let status;
@@ -40,7 +41,9 @@ function App() {
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice(); //slice()하는 이유는 원본을 바로 수정하는 것이 아니라 복사본을 만들어서 수정한 다음 넣어주기 위해 사용
+    const newHistory = history.slice(0, stepNumber + 1); //history의 0부터 +1까지를 복사
+    const newCurrent = newHistory[newHistory.length - 1]; //newHistory의 현재 아이템만 갖게 된다.
+    const newSquares = newCurrent.squares.slice(); //slice()하는 이유는 원본을 바로 수정하는 것이 아니라 복사본을 만들어서 수정한 다음 넣어주기 위해 사용
     console.log('newSquares', newSquares);
     console.log('newSquares[i] : ', newSquares[i]);
     
@@ -50,8 +53,15 @@ function App() {
     }
 
     newSquares[i] = xIsNext ? 'X' : 'O';
-    setHistory([...history, {squares: newSquares}] ); //...은 전개 연산자, history에 newSquares를 push한다.
+    setHistory([...newHistory, {squares: newSquares}] ); //...은 전개 연산자, history에 newSquares를 push한다.
     setXIsNext(prev => !prev);
+
+    setStepNumber(newHistory.length);
+  }
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0);
   }
 
   const moves = history.map((step, move) => {
@@ -60,7 +70,7 @@ function App() {
       'Go to Game start';    //인덱스가 0일 때 (0은 false니까)
     return (
       <li key={move}>
-        <button>{desc}</button>
+        <button className="move-button" onClick={() => jumpTo(move)}>{desc}</button>
       </li>
     );
   });
@@ -72,7 +82,7 @@ function App() {
       </div>
       <div className="game-info">
         <div className='status'>{status}</div>
-        <ol>{moves}</ol> 
+        <ol style={{ listStyle : 'none' }}>{moves}</ol> 
       </div>
       
     </div>
